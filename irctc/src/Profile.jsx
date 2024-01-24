b7fce1fa88f9eab1ca0e725e81a684b0a1ed578a
KureshiAzahar <202101187@daiict.ac.in>
2024-01-25 01:39:25 +0530

    Profile Page

--- irctc/src/Profile.jsx
+++ irctc/src/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, get, child, onValue } from "firebase/database";
 import { app } from "./FirebaseConfig";
+import { Link } from "react-router-dom"
 
 
 const databas = getDatabase(app);
@@ -8,9 +9,9 @@ const databas = getDatabase(app);
 function setdata() {
     // Corrected syntax and added then/catch to handle the asynchronous nature of set function
     set(ref(databas, "/Profile/user"), {
-      id: 10,
-      name: "Azahar",
-      age: 21,
+      id: 11,
+      name: "Kureshi",
+      age: 20,
     })
       .then(() => {
         console.log("Data successfully set");
@@ -20,12 +21,39 @@ function setdata() {
       });
   }
 
+function getdata(){
+  // get(child(ref(databas), "Profile"))
+  // .then(snapshot => console.log(snapshot.val()))
+  // .catch(err => console.error(err))
+
+  onValue(ref(databas,"Profile/user"),snapshot => console.log(snapshot.val()))
+}
+
 export const Profile = () => {
     return (
         <div className="ProfileContainer">
+          <div className="userphoto">
+            <img src="./background-image.jpg" alt="NO Preview"/>
+          </div>
             <div className="UserData">
-                <p>Profile</p>
-                <button onClick={setdata}>Submit</button>
+                {/* <p>Profile</p> */}
+                <label>First Name</label>
+                <div>Azahar</div>
+                <label>Last Name</label>
+                <div>Kureshi</div>
+                <label>Username</label>
+                <div>Azahar Kureshi</div>
+                <label>Email</label>
+                <div>sample@gmail.com</div>
+                <label>Gender</label>
+                <div>Male</div>
+                <label>Date Of Birth</label>
+                <div>01/10/2003</div>
+                <label>Phone Number</label>
+                <div>6351305430</div>
+                <Link to={"/editprofile"}>Edit Profile</Link>
+                {/* <button onClick={setdata}>Submit</button>
+                <button onClick={getdata}>Fetch Data</button> */}
             </div>
         </div>
     );
